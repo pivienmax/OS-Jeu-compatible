e2bf3af3b508b6ea4d08a77a9c1582137023c4cd
Berchigadov Dokka <dokka.berchigadov@etu.univ-orleans.fr>
2024-10-09 08:36:15 +0200

    fichier test avec vm et asm

---
+++ VMTranslator/test/test.asm
@@ -0,0 +1,12 @@
+// Bootstrap
+    @256
+    D=A
+    @SP
+    M=D
+	//Call Sys.init 0
+    Code assembleur de {'type': 'Call', 'function': 'Sys.init', 'parameter': '0'}
+
+
+//code de test/test.vm
+	//push constant 7
+    Code assembleur de {'line': 1, 'col': 1, 'type': 'push', 'segment': 'constant', 'parameter': '7'}
